fabe68f257a9c050f30c5ff82cc3783256d036dc
begojara99 <pch990824@naver.com>
2024-06-07 13:49:35 +0900

    [FEATURE] 지원서 목록 조회 체크 방지 기능 작업 #64

--- src/APP/admin-pages/ViewApplicationList/ViewApplicationList.viewapplicationlist.table.jsx
+++ src/APP/admin-pages/ViewApplicationList/ViewApplicationList.viewapplicationlist.table.jsx
@@ -2,60 +2,58 @@ import React, { useState } from 'react';
 import * as itemS from "./Styled/ViewApplicationList.viewapplicationlist.table.styles";
 import ViewApplicationListTuple from './ViewApplicationList.viewapplicationlist.tuple';
 
-export default function ViewApplicationListTable({ applications, onCheckChange }) {
-	const [selectedApplicationId, setSelectedApplicationId] = useState(null); // 선택된 지원자의 ID를 저장하는 상태
-	const [isModalOpen, setIsModalOpen] = useState(false); // 모달 창의 열림/닫힘 상태를 저장하는 상태
-	const [selectAll, setSelectAll] = useState(false); // 전체 선택 체크박스 상태를 저장하는 상태
+export default function ViewApplicationListTable({ applications, onCheckChange, firstCheckedStage }) {
+    const [selectedApplicationId, setSelectedApplicationId] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectAll, setSelectAll] = useState(false);
 
-	// 모달 창을 닫는 함수
-	const closeModal = () => {
-		setIsModalOpen(false); // 모달 창 닫기
-		setSelectedApplicationId(null); // 선택된 지원자 ID 초기화
-	};
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedApplicationId(null);
+    };
 
-	// 모달 창을 여는 함수
-	const openModal = (applicationId) => {
-		setIsModalOpen(true); // 모달 창 열기
-		setSelectedApplicationId(applicationId); // 선택된 지원자 ID 설정
-	};
+    const openModal = (applicationId) => {
+        setIsModalOpen(true);
+        setSelectedApplicationId(applicationId);
+    };
 
-	// 전체 선택 체크박스 변경 시 호출되는 함수
-	const handleSelectAllChange = (event) => {
-		const isChecked = event.target.checked;
-		setSelectAll(isChecked); // 전체 선택 상태 설정
-		applications.forEach(application => onCheckChange(application.id, isChecked)); // 모든 지원자에 대해 체크 상태 변경
-	};
+    const handleSelectAllChange = (event) => {
+        const isChecked = event.target.checked;
+        setSelectAll(isChecked);
+        applications.forEach(application => onCheckChange(application.id, isChecked, application.selection_stage));
+    };
 
-	return (
-		<itemS.Container>
-			<itemS.Table>
-				<itemS.CategoryContainer>
-					<itemS.CheckBox
-						type="checkbox"
-						checked={selectAll}
-						onChange={handleSelectAllChange} // 전체 선택 체크박스 변경 핸들러
-					/>
-					<itemS.CategoryShort>이름</itemS.CategoryShort>
-					<itemS.CategoryShort>학년</itemS.CategoryShort>
-					<itemS.CategoryLong>학과</itemS.CategoryLong>
-					<itemS.Category>참여 희망 스터디</itemS.Category>
-					<itemS.Category>진행 단계</itemS.Category>
-					<itemS.CategoryDrop>면접 일정</itemS.CategoryDrop>
-				</itemS.CategoryContainer>
-				<itemS.TupleContainer>
-					{applications.map(application => (
-						<ViewApplicationListTuple
-							key={application.id} // 각 지원자의 고유 ID를 키로 사용
-							application={application} // 지원자 데이터 전달
-							isSelected={selectedApplicationId === application.id && isModalOpen} // 선택된 지원자인지 여부와 모달 창 상태
-							onOpen={() => openModal(application.id)} // 모달 창 여는 함수
-							onClose={closeModal} // 모달 창 닫는 함수
-							onCheckChange={onCheckChange} // 체크박스 상태 변경 함수
-							selectAll={selectAll} // 전체 선택 상태
-						/>
-					))}
-				</itemS.TupleContainer>
-			</itemS.Table>
-		</itemS.Container>
-	);
+    return (
+        <itemS.Container>
+            <itemS.Table>
+                <itemS.CategoryContainer>
+                    <itemS.CheckBox
+                        type="checkbox"
+                        checked={selectAll}
+                        onChange={handleSelectAllChange}
+                    />
+                    <itemS.CategoryShort>이름</itemS.CategoryShort>
+                    <itemS.CategoryShort>학년</itemS.CategoryShort>
+                    <itemS.CategoryLong>학과</itemS.CategoryLong>
+                    <itemS.Category>참여 희망 스터디</itemS.Category>
+                    <itemS.Category>진행 단계</itemS.Category>
+                    <itemS.CategoryDrop>면접 일정</itemS.CategoryDrop>
+                </itemS.CategoryContainer>
+                <itemS.TupleContainer>
+                    {applications.map(application => (
+                        <ViewApplicationListTuple
+                            key={application.id}
+                            application={application}
+                            isSelected={selectedApplicationId === application.id && isModalOpen}
+                            onOpen={() => openModal(application.id)}
+                            onClose={closeModal}
+                            onCheckChange={onCheckChange}
+                            selectAll={selectAll}
+                            firstCheckedStage={firstCheckedStage}
+                        />
+                    ))}
+                </itemS.TupleContainer>
+            </itemS.Table>
+        </itemS.Container>
+    );
 }

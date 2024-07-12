9e979a1689d5fe03b97274da62f2d34012a9dc38
begojara99 <pch990824@naver.com>
2024-07-12 22:45:49 +0900

    [DESIGN] 7월 9일 지원서 양식 목록 QA 작업 #120

--- src/APP/admin-pages/MakedApplicationList/MakedApplicationList.makedapplicationlist.individual.jsx
+++ src/APP/admin-pages/MakedApplicationList/MakedApplicationList.makedapplicationlist.individual.jsx
@@ -37,11 +37,11 @@ export default function MakedApplicationListIndividual({ application, setCntApp 
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
+    // const hours = String(date.getHours()).padStart(2, '0');
+    // const minutes = String(date.getMinutes()).padStart(2, '0');
+    // const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day}`;
   };
 
   return (

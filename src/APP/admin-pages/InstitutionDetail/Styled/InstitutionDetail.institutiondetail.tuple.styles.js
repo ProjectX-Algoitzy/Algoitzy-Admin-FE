9a58fa514ed7b177859228c793040da0388b9792
begojara99 <pch990824@naver.com>
2024-07-30 03:59:41 +0900

    [FEATURE][DESIGN] 기업/부트캠프 상세 조회 화면 작업 #151

---
+++ src/APP/admin-pages/InstitutionDetail/Styled/InstitutionDetail.institutiondetail.tuple.styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import * as tokens from "../../../../tokens"
+
+
+// 튜플 파트 시작
+export const TupleContainer = styled.div`
+  display: flex;
+  // position: relative;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 1200px;
+  // height: 55px;
+  
+  border-bottom: 1px solid ${tokens.colors.B_Grey_3};
+  &:hover {
+    background-color: ${tokens.colors.B_Grey_1};
+  }
+  cursor: pointer;
+`;
+
+export const TupleNumber = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  ${tokens.typography.T5_SB_16};
+  color: ${tokens.colors.Black};
+  width: 72px;
+  height: 56px;
+`;
+
+export const TupleTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  ${tokens.typography.T5_SB_16};
+  color: ${tokens.colors.Black};
+  width: 200px;
+  height: 56px;
+  margin-left: 161px;
+`;
+
+
+

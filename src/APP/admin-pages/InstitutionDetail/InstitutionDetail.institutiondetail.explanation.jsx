fa531aadf2224b2c7dd5807605dbeb37afa95b6b
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2024-08-23 22:00:37 +0900

    [Error] 코딩테스트 분석 제작시 에디터의 줄바꿈 에러 #223

--- src/APP/admin-pages/InstitutionDetail/InstitutionDetail.institutiondetail.explanation.jsx
+++ src/APP/admin-pages/InstitutionDetail/InstitutionDetail.institutiondetail.explanation.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import * as itemS from "./Styled/InstitutionDetail.institutiondetail.explanation.styles";
 
 export default function InstitutionDetailExplanation({ content }) {
-   
+   console.log("ㄴ야ㅓ랸얼content: ", content);
     
   return (
     <itemS.Container>
-        <itemS.CategoryContainer dangerouslySetInnerHTML={{ __html: content }}></itemS.CategoryContainer>
+        <itemS.CategoryContainer>
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        </itemS.CategoryContainer>
     </itemS.Container>
   );
 }

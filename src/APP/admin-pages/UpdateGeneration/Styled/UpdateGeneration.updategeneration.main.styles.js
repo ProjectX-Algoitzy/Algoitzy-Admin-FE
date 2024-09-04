2cf93d125ce3b9c75ac10f834358c650cd841a2e
begojara99 <pch990824@naver.com>
2024-09-04 21:21:27 +0900

    [DESIGN] 기수 갱신 PX -> REM 변경

--- src/APP/admin-pages/UpdateGeneration/Styled/UpdateGeneration.updategeneration.main.styles.js
+++ src/APP/admin-pages/UpdateGeneration/Styled/UpdateGeneration.updategeneration.main.styles.js
@@ -1,8 +1,6 @@
 import styled, { css } from 'styled-components';
 import * as tokens from "../../../../tokens"
 
-
-
 export const OuterContainer = styled.div`
   position: relative;
   width: 100%;
@@ -10,7 +8,6 @@ export const OuterContainer = styled.div`
 
 export const Container = styled.div`
   display: flex;
-  // flex-direction: row;
   justify-content: center; 
 `;
 
@@ -18,17 +15,17 @@ export const InnerContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center; 
-  border-radius: 20px;
-  padding: 158px 360px 0px 360px;
-  margin-bottom: 98px;
+  border-radius: 0.833rem;
+  padding: 6.583rem 15rem 0rem 15rem;
+  margin-bottom: 4.083rem;
 `;
 
 export const HeadContainer = styled.div`
   display: flex;
   justify-content: flex-start;
-  width: 1200px;
+  width: 50rem;
   border-bottom: 1px solid ${tokens.colors.B_Grey_2};
-  margin-bottom: 12px;
+  margin-bottom: 0.5rem;
 `;
 
 export const CautionContainer = styled.div`
@@ -37,15 +34,15 @@ export const CautionContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   background-color: ${tokens.colors.B_Grey_1};
-  width: 1200px;
-  height: 77px;
-  border-radius: 10px;
-  margin-bottom: 16px;
+  width: 50rem;
+  height: 3.208rem;
+  border-radius: 0.417rem;
+  margin-bottom: 0.667rem;
 `;
 
 export const Head = styled.div`
   ${tokens.typography.T3_B_24};
-  margin-bottom: 20px;
+  margin-bottom: 0.833rem;
 `;
 
 export const Caution = styled.div`
@@ -54,21 +51,20 @@ export const Caution = styled.div`
 `;
 
 export const CautionIcon = styled.img`
-  width: 24px;
-  height: 24px;
-  margin-left: 20px;
-  margin-right: 8px;
+  width: 1rem;
+  height: 1rem;
+  margin-left: 0.833rem;
+  margin-right: 0.333rem;
 `;
 
 export const UdateButton = styled.button`
-  width: 233px;
-  height: 48px;
+  width: 9.708rem;
+  height: 2rem;
   background-color: ${tokens.colors.Red};
   ${tokens.typography.T5_SB_16};
   color: ${tokens.colors.White};
   border: none;
-  border-radius: 4px;
-  margin-top: 56px;
+  border-radius: 0.167rem;
+  margin-top: 2.333rem;
   cursor: pointer;
 `;
-
--- src/APP/admin-pages/UpdateGeneration/Styled/UpdateModal.js
+++ src/APP/admin-pages/UpdateGeneration/Styled/UpdateModal.js
@@ -4,228 +4,217 @@ import Select from 'react-select';
 
 export const ModalContainer = styled.div`
   position: absolute;
-  left: 33%;
+  left: 16.75rem;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  width: 280px;
-  height: 51px;
+  width: 11.667rem;
+  height: 2.125rem;
   background: white;
-  padding: 0 12px;
-  border-radius: 4px;
-  box-shadow: 0 4px 10px 4px rgba(58, 107, 135, 0.08);
+  padding: 0 0.5rem;
+  border-radius: 0.167rem;
+  box-shadow: 0 0.167rem 0.417rem 0.167rem rgba(58, 107, 135, 0.08);
   z-index: 1001;
 `;
 
 export const DropText = styled.div`
   ${tokens.typography.B2_M_16};
   color: ${tokens.colors.Black};
-  margin: 0 4px;
+  margin: 0 0.167rem;
 `;
 
 export const ConfirmButton = styled.button`
-  width: 40px;
-  height: 24px;
+  width: 1.667rem;
+  height: 1rem;
   background-color: ${tokens.colors.B_Grey_7};
   color: ${tokens.colors.White};
   border: none;
   padding: 0;
   cursor: pointer;
-  border-radius: 4px;
+  border-radius: 0.167rem;
 `;
 
 export const YearDateSelect = styled(Select).attrs({
   classNamePrefix: 'react-select',
 })`
-.react-select__control {
-  // width: 42px;
-  // height: 24px;
-  color: ${tokens.colors.Grey_8};
-  ${tokens.typography.B3_M_14};
-  border: 1px solid ${tokens.colors.B_Grey_3};
-  border-radius: 4px;
-  text-align: center;
-  justify-content: center;
-}
-
-.react-select__menu {
-  position: absolute;
-  top: -10px;  
-  left: -2px;
-  width: 60px;
-  height: 76px; 
-  border-radius: 4px;
-  border: none;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  font-weight: 600;
-  text-align: center;
-  ${tokens.typography.B3_M_14};
-  overflow: hidden; /* Hide scrollbar */
-}
-
-.react-select__menu-list {
-  max-height: 244px;
-  overflow-y: auto;
-  /* Hide scrollbar for WebKit-based browsers (Chrome, Safari, etc.) */
-  &::-webkit-scrollbar {
-    width: 0;
-    height: 0;
+  .react-select__control {
+    color: ${tokens.colors.Grey_8};
+    ${tokens.typography.B3_M_14};
+    border: 1px solid ${tokens.colors.B_Grey_3};
+    border-radius: 0.167rem;
+    text-align: center;
+    justify-content: center;
   }
-  /* Hide scrollbar for Firefox */
-  scrollbar-width: none;
-  /* Hide scrollbar for Internet Explorer and Edge */
-  -ms-overflow-style: none;
-}
 
-.react-select__option:not(:last-child) {
-  border-bottom: 1px solid ${tokens.colors.B_Grey_2};
-}
+  .react-select__menu {
+    position: absolute;
+    top: -0.417rem;  
+    left: -0.083rem;
+    width: 2.5rem;
+    height: 3.167rem; 
+    border-radius: 0.167rem;
+    border: none;
+    box-shadow: 0 0.083rem 0.167rem rgba(0, 0, 0, 0.1);
+    font-weight: 600;
+    text-align: center;
+    ${tokens.typography.B3_M_14};
+    overflow: hidden; /* Hide scrollbar */
+  }
 
-.react-select__option {
-  color: ${tokens.colors.Grey_8};
-  ${tokens.typography.B3_M_14};
-  border: none;
-}
+  .react-select__menu-list {
+    max-height: 10.167rem;
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+      width: 0;
+      height: 0;
+    }
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
 
-.react-select__option--is-selected:first-of-type {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  border: none;
-  ${tokens.typography.B3_M_14};
-  position: relative;
-  top: -4px;
-}
+  .react-select__option:not(:last-child) {
+    border-bottom: 1px solid ${tokens.colors.B_Grey_2};
+  }
 
-.react-select__option--is-selected:last-of-type {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border: none;
-  ${tokens.typography.B3_M_14};
-}
+  .react-select__option {
+    color: ${tokens.colors.Grey_8};
+    ${tokens.typography.B3_M_14};
+    border: none;
+  }
 
-.react-select__option--is-selected:not(:first-of-type):not(:last-of-type) {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border: none;
-  ${tokens.typography.B3_M_14};
-}
+  .react-select__option--is-selected:first-of-type {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border-top-left-radius: 0.167rem;
+    border-top-right-radius: 0.167rem;
+    border: none;
+    ${tokens.typography.B3_M_14};
+    position: relative;
+    top: -0.167rem;
+  }
 
-.react-select__option--is-focused {
-  background-color: transparent;
-  cursor: pointer;
-}
+  .react-select__option--is-selected:last-of-type {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border-bottom-left-radius: 0.167rem;
+    border-bottom-right-radius: 0.167rem;
+    border: none;
+    ${tokens.typography.B3_M_14};
+  }
 
-.react-select__option:active {
-  background-color: transparent;
-}
+  .react-select__option--is-selected:not(:first-of-type):not(:last-of-type) {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border: none;
+    ${tokens.typography.B3_M_14};
+  }
 
-.react-select__option:hover {
-  background-color: rgba(102, 201, 255, 0.2);
-}
+  .react-select__option--is-focused {
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .react-select__option:active {
+    background-color: transparent;
+  }
+
+  .react-select__option:hover {
+    background-color: rgba(102, 201, 255, 0.2);
+  }
 `;
-
 
 export const MonthDateSelect = styled(Select).attrs({
   classNamePrefix: 'react-select',
 })`
-.react-select__control {
-  // width: 42px;
-  // height: 24px;
-  color: ${tokens.colors.Grey_8};
-  ${tokens.typography.B3_M_14};
-  border: 1px solid ${tokens.colors.B_Grey_3};
-  border-radius: 4px;
-  text-align: center;
-  justify-content: center;
-}
-
-.react-select__menu {
-  position: absolute;
-  top: -10px;  
-  left: -2px;
-  width: 44px;
-  height: 250px; 
-  border-radius: 4px;
-  border: none;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  font-weight: 600;
-  text-align: center;
-  ${tokens.typography.B3_M_14};
-  overflow: hidden; /* Hide scrollbar */
-}
-
-.react-select__menu-list {
-  max-height: 244px;
-  overflow-y: auto;
-  /* Hide scrollbar for WebKit-based browsers (Chrome, Safari, etc.) */
-  &::-webkit-scrollbar {
-    width: 0;
-    height: 0;
+  .react-select__control {
+    color: ${tokens.colors.Grey_8};
+    ${tokens.typography.B3_M_14};
+    border: 1px solid ${tokens.colors.B_Grey_3};
+    border-radius: 0.167rem;
+    text-align: center;
+    justify-content: center;
   }
-  /* Hide scrollbar for Firefox */
-  scrollbar-width: none;
-  /* Hide scrollbar for Internet Explorer and Edge */
-  -ms-overflow-style: none;
-}
 
-.react-select__option:not(:last-child) {
-  border-bottom: 1px solid ${tokens.colors.B_Grey_2};
-}
+  .react-select__menu {
+    position: absolute;
+    top: -0.417rem;  
+    left: -0.083rem;
+    width: 1.833rem;
+    height: 10.417rem; 
+    border-radius: 0.167rem;
+    border: none;
+    box-shadow: 0 0.083rem 0.167rem rgba(0, 0, 0, 0.1);
+    font-weight: 600;
+    text-align: center;
+    ${tokens.typography.B3_M_14};
+    overflow: hidden; /* Hide scrollbar */
+  }
 
-.react-select__option {
-  color: ${tokens.colors.Grey_8};
-  ${tokens.typography.B3_M_14};
-  border: none;
-}
+  .react-select__menu-list {
+    max-height: 10.167rem;
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+      width: 0;
+      height: 0;
+    }
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
 
-.react-select__option--is-selected:first-of-type {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  border: none;
-  ${tokens.typography.B3_M_14};
-  position: relative;
-  top: -4px;
-}
+  .react-select__option:not(:last-child) {
+    border-bottom: 1px solid ${tokens.colors.B_Grey_2};
+  }
 
-.react-select__option--is-selected:last-of-type {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border: none;
-  ${tokens.typography.B3_M_14};
-}
+  .react-select__option {
+    color: ${tokens.colors.Grey_8};
+    ${tokens.typography.B3_M_14};
+    border: none;
+  }
 
-.react-select__option--is-selected:not(:first-of-type):not(:last-of-type) {
-  background-color: ${tokens.colors.White};
-  backdrop-filter: blur(8px);
-  color: ${tokens.colors.Grey_8};
-  border: none;
-  ${tokens.typography.B3_M_14};
-}
+  .react-select__option--is-selected:first-of-type {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border-top-left-radius: 0.167rem;
+    border-top-right-radius: 0.167rem;
+    border: none;
+    ${tokens.typography.B3_M_14};
+    position: relative;
+    top: -0.167rem;
+  }
 
-.react-select__option--is-focused {
-  background-color: transparent;
-  cursor: pointer;
-}
+  .react-select__option--is-selected:last-of-type {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border-bottom-left-radius: 0.167rem;
+    border-bottom-right-radius: 0.167rem;
+    border: none;
+    ${tokens.typography.B3_M_14};
+  }
 
-.react-select__option:active {
-  background-color: transparent;
-}
+  .react-select__option--is-selected:not(:first-of-type):not(:last-of-type) {
+    background-color: ${tokens.colors.White};
+    backdrop-filter: blur(8px);
+    color: ${tokens.colors.Grey_8};
+    border: none;
+    ${tokens.typography.B3_M_14};
+  }
 
-.react-select__option:hover {
-  background-color: rgba(102, 201, 255, 0.2);
-}
+  .react-select__option--is-focused {
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .react-select__option:active {
+    background-color: transparent;
+  }
+
+  .react-select__option:hover {
+    background-color: rgba(102, 201, 255, 0.2);
+  }
 `;

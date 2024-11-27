2708b01038781e757ca7ec087a182ace3cf662d9
begojara99 <pch990824@naver.com>
2024-11-27 20:02:38 +0900

    커뮤니티 파일 다운로드

--- src/APP/admin-pages/BoardDetail/Styled/BoardDetail.boarddetail.content.styles.js
+++ src/APP/admin-pages/BoardDetail/Styled/BoardDetail.boarddetail.content.styles.js
@@ -49,5 +49,14 @@ export const FileList = styled.div`
 
 export const File = styled.div`
   ${tokens.typography.B2_M_16};
-  color: ${tokens.colors.Black};
+  // color: ${tokens.colors.Black};
+
+  a {
+    text-decoration: none;
+    color: ${tokens.colors.Black};
+
+    // &:hover {
+    //   color: ${tokens.colors.Primary};
+    // }
+  }
 `;

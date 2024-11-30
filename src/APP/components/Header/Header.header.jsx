5b66ca935498630c2cc3b6286137c6e03b05b2de
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2024-11-30 19:58:40 +0900

    [FIX] 헤더 너비 맞추기 (342브랜치의 오류로 재생성) #344

--- src/APP/components/Header/Header.header.jsx
+++ src/APP/components/Header/Header.header.jsx
@@ -81,74 +81,76 @@ export default function Header() {
 
   return (
     <itemS.HeaderContainer activeMenu={activeMenu}>
-      <itemS.HeaderWrap>
-        <itemS.HeaderLeftWrap>
-          <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} style={{textDecoration: 'none'}}><itemS.Rabel src='/img/koalalogo.png' alt='코알라로고'/></itemS.StyledLink>
-        </itemS.HeaderLeftWrap>
-        <itemS.HeaderRightWrap>
-          <itemS.StyledLink onClick={() => handleMenuClick('application')}>
-            <itemS.PageLink>지원자 관리</itemS.PageLink>
-            {activeMenu === 'application' && (
-              <itemS.SubMenuContaier ref={applicationMenuRef} onClick={(e) => e.stopPropagation()}>
-                <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                  <itemS.StyledLink to={isLoggedIn ? "/manageauth" : "/login"} onClick={handleNav}>
-                    <itemS.SubMenuItem>권한 관리</itemS.SubMenuItem>
-                  </itemS.StyledLink>
-                  <itemS.StyledLink to={isLoggedIn ? "/answer" : "/login"} onClick={handleNav}>
-                    <itemS.SubMenuItem>지원자 관리</itemS.SubMenuItem>
-                  </itemS.StyledLink>
-                </itemS.SubMenu>
-              </itemS.SubMenuContaier>
-            )}
-          </itemS.StyledLink> 
-          <itemS.StyledLink onClick={() => handleMenuClick('study')}>
-            <itemS.PageLink>스터디 관리</itemS.PageLink>
-            {activeMenu === 'study' && (
-              <itemS.SubMenuContaier ref={studyMenuRef} onClick={(e) => e.stopPropagation()}>
-                <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                  <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} onClick={handleNav}>
-                    <itemS.SubMenuItem>정규 스터디</itemS.SubMenuItem>
-                  </itemS.StyledLink>
-                  <itemS.StyledLink to={isLoggedIn ? "/generation" : "/login"} onClick={handleNav}>
-                    <itemS.SubMenuItem>기수 갱신</itemS.SubMenuItem>
-                  </itemS.StyledLink>
-                </itemS.SubMenu>
-              </itemS.SubMenuContaier>
-            )}
-          </itemS.StyledLink>
-          {/* <itemS.StyledLink>
-            <itemS.PageLink>커뮤니티</itemS.PageLink>
-          </itemS.StyledLink>
-          <itemS.StyledLink>
-            <itemS.PageLink>문의 관리</itemS.PageLink>
-          </itemS.StyledLink> */}
-          <itemS.StyledLink onClick={() => handleMenuClick('coding')}>
-            <itemS.PageLink>코딩테스트 분석</itemS.PageLink>
-            {activeMenu === 'coding' && (
-              <itemS.SubMenuContaier ref={codingMenuRef} onClick={(e) => e.stopPropagation()}>
-                <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                  <itemS.StyledLink to={isLoggedIn ? "/enterbootlist" : "/login"} onClick={handleNav}>
-                    <itemS.SubMenuItem style={{marginBottom:"2.5rem"}}>기업/부트캠프</itemS.SubMenuItem>
-                  </itemS.StyledLink>
-                </itemS.SubMenu>
-              </itemS.SubMenuContaier>
-            )}
-          </itemS.StyledLink>
-          {isLoggedIn ? (
-            <div style={{ position: 'relative' }} ref={modalRef}>
-              <itemS.ProfileBox onClick={toggleProfileModal}>
-                <itemS.AdminName>안녕하세요, {userName} 님</itemS.AdminName>
-                <itemS.Arrow src='/img/arrow-bt.svg' alt='화살표' />
-              </itemS.ProfileBox>
-              {showProfileModal && <ProfileModal userName={userName} profileUrl={profileUrl} setIsLoggedIn={setIsLoggedIn}/>}
-            </div>
-          ) : (
-            <itemS.StyledLink to="/login">
-              <itemS.Btn>로그인/회원가입</itemS.Btn>
+      <itemS.InnerContainer>
+        <itemS.HeaderWrap>
+          <itemS.HeaderLeftWrap>
+            <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} style={{textDecoration: 'none'}}><itemS.Rabel src='/img/koalalogo.png' alt='코알라로고'/></itemS.StyledLink>
+          </itemS.HeaderLeftWrap>
+          <itemS.HeaderRightWrap>
+            <itemS.StyledLink onClick={() => handleMenuClick('application')}>
+              <itemS.PageLink>지원자 관리</itemS.PageLink>
+              {activeMenu === 'application' && (
+                <itemS.SubMenuContaier ref={applicationMenuRef} onClick={(e) => e.stopPropagation()}>
+                  <itemS.SubMenu isLoggedIn={isLoggedIn}>
+                    <itemS.StyledLink to={isLoggedIn ? "/manageauth" : "/login"} onClick={handleNav}>
+                      <itemS.SubMenuItem>권한 관리</itemS.SubMenuItem>
+                    </itemS.StyledLink>
+                    <itemS.StyledLink to={isLoggedIn ? "/answer" : "/login"} onClick={handleNav}>
+                      <itemS.SubMenuItem>지원자 관리</itemS.SubMenuItem>
+                    </itemS.StyledLink>
+                  </itemS.SubMenu>
+                </itemS.SubMenuContaier>
+              )}
+            </itemS.StyledLink> 
+            <itemS.StyledLink onClick={() => handleMenuClick('study')}>
+              <itemS.PageLink>스터디 관리</itemS.PageLink>
+              {activeMenu === 'study' && (
+                <itemS.SubMenuContaier ref={studyMenuRef} onClick={(e) => e.stopPropagation()}>
+                  <itemS.SubMenu isLoggedIn={isLoggedIn}>
+                    <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} onClick={handleNav}>
+                      <itemS.SubMenuItem>정규 스터디</itemS.SubMenuItem>
+                    </itemS.StyledLink>
+                    <itemS.StyledLink to={isLoggedIn ? "/generation" : "/login"} onClick={handleNav}>
+                      <itemS.SubMenuItem>기수 갱신</itemS.SubMenuItem>
+                    </itemS.StyledLink>
+                  </itemS.SubMenu>
+                </itemS.SubMenuContaier>
+              )}
             </itemS.StyledLink>
-          )}
-        </itemS.HeaderRightWrap>
-      </itemS.HeaderWrap>
+            <itemS.StyledLink to={isLoggedIn ? "/community" : "/login"}>
+              <itemS.PageLink>커뮤니티</itemS.PageLink>
+            </itemS.StyledLink>
+            {/* <itemS.StyledLink>
+              <itemS.PageLink>문의 관리</itemS.PageLink>
+            </itemS.StyledLink> */}
+            <itemS.StyledLink onClick={() => handleMenuClick('coding')}>
+              <itemS.PageLink>코딩테스트 분석</itemS.PageLink>
+              {activeMenu === 'coding' && (
+                <itemS.SubMenuContaier ref={codingMenuRef} onClick={(e) => e.stopPropagation()}>
+                  <itemS.SubMenu isLoggedIn={isLoggedIn}>
+                    <itemS.StyledLink to={isLoggedIn ? "/enterbootlist" : "/login"} onClick={handleNav}>
+                      <itemS.SubMenuItem style={{marginBottom:"2.5rem"}}>기업/부트캠프</itemS.SubMenuItem>
+                    </itemS.StyledLink>
+                  </itemS.SubMenu>
+                </itemS.SubMenuContaier>
+              )}
+            </itemS.StyledLink>
+            {isLoggedIn ? (
+              <div style={{ position: 'relative' }} ref={modalRef}>
+                <itemS.ProfileBox onClick={toggleProfileModal}>
+                  <itemS.AdminName>안녕하세요, {userName} 님</itemS.AdminName>
+                  <itemS.Arrow src='/img/arrow-bt.svg' alt='화살표' />
+                </itemS.ProfileBox>
+                {showProfileModal && <ProfileModal userName={userName} profileUrl={profileUrl} setIsLoggedIn={setIsLoggedIn}/>}
+              </div>
+            ) : (
+              <itemS.StyledLink to="/login">
+                <itemS.Btn>로그인/회원가입</itemS.Btn>
+              </itemS.StyledLink>
+            )}
+          </itemS.HeaderRightWrap>
+        </itemS.HeaderWrap>
+      </itemS.InnerContainer>
     </itemS.HeaderContainer>
   );
 }

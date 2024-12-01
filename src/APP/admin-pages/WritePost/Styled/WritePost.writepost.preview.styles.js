2d0ea0dd9169b10bd2d5e0fcafd4771d3ad0381a
nunomi0 <ykys1@naver.com>
2024-12-02 03:33:00 +0900

    마크다운 인용 디자인 수정

--- src/APP/admin-pages/WritePost/Styled/WritePost.writepost.preview.styles.js
+++ src/APP/admin-pages/WritePost/Styled/WritePost.writepost.preview.styles.js
@@ -77,9 +77,10 @@ export const PreviewContainer = styled.div`
     font-size: 0.9rem;
   }
   blockquote {
-    border-left: 0.2rem solid ${tokens.colors.B_Grey_2};
     padding-left: 1rem;
-    color: #666;
+    border-left: 0.2rem solid ${tokens.colors.Blue_0_Main};
+    background-color: ${tokens.colors.B_Grey_2}; /* 배경색 추가 */
+    color: ${tokens.colors.B_Grey_6};
     font-style: italic;
     margin: 1rem 0;
   }

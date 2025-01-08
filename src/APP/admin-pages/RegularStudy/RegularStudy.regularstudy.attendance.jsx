5a11dc8ab7d6f686707ced7efb11d86835a5c544
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2025-01-08 15:49:13 +0900

    출석부 갱신 alert 메시지 수정

--- src/APP/admin-pages/RegularStudy/RegularStudy.regularstudy.attendance.jsx
+++ src/APP/admin-pages/RegularStudy/RegularStudy.regularstudy.attendance.jsx
@@ -236,7 +236,7 @@ export default function RegularStudyAttendance() {
 
       const response = await request.patch(`/attendance`, { attendanceList: updatedDataForServer });
       if (response.isSuccess) {
-        await alert("출석 정보가 성공적으로 갱신되었습니다.");
+        await alert("출석부가 갱신되었습니다");
         setIsModified(false);
         setModifiedAttendance([]);
         window.location.reload();

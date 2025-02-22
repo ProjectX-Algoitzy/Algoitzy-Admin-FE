d5d99b245b2850ab8beb2d5ef29962c0a57c135f
begojara99 <pch990824@naver.com>
2025-02-22 16:58:26 +0900

    계정 찾기 관련 페이지 삭제

--- src/APP/components/Header/Header.header.jsx
+++ src/APP/components/Header/Header.header.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import * as itemS from "./Styled/Header.header.style";
-import request from '../../Api/request';
-import ProfileModal from './Header.profile.modal'; 
+import request from "../../Api/request";
+import ProfileModal from "./Header.profile.modal";
 
 export default function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userName, setUserName] = useState('');
-  const [profileUrl, setProfileUrl] = useState('');
+  const [userName, setUserName] = useState("");
+  const [profileUrl, setProfileUrl] = useState("");
   const [showProfileModal, setShowProfileModal] = useState(false);
-  const [activeMenu, setActiveMenu] = useState(''); 
+  const [activeMenu, setActiveMenu] = useState("");
 
   // Refs for detecting clicks outside
-  const modalRef = useRef(null); 
+  const modalRef = useRef(null);
   const studyMenuRef = useRef(null);
   const applicationMenuRef = useRef(null);
   const codingMenuRef = useRef(null);
 
   const checkLoginStatus = async () => {
     try {
-      const response = await request.get('/member/info');
+      const response = await request.get("/member/info");
       console.log("로그인 멤버 정보 조회", response);
       if (response["isSuccess"]) {
         setUserName(response.result.name);
@@ -30,56 +30,60 @@ export default function Header() {
     }
   };
   useEffect(() => {
-
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       checkLoginStatus();
     }
   }, []);
 
   const toggleProfileModal = () => {
-    setShowProfileModal(prev => !prev);
-    setActiveMenu('');
+    setShowProfileModal((prev) => !prev);
+    setActiveMenu("");
   };
 
   const handleMenuClick = (menu) => {
-    setActiveMenu((prev) => (prev === menu ? '' : menu));
+    setActiveMenu((prev) => (prev === menu ? "" : menu));
   };
 
-  const handleNav = () => {  // 하위 메뉴 이동 후 메뉴창 닫기
-    setActiveMenu('');
+  const handleNav = () => {
+    // 하위 메뉴 이동 후 메뉴창 닫기
+    setActiveMenu("");
   };
 
   // Click Outside Handler
   useEffect(() => {
     const handleClickOutside = (event) => {
       // Check if click is inside Profile Modal
-      const clickedInsideProfile = modalRef.current && modalRef.current.contains(event.target);
+      const clickedInsideProfile =
+        modalRef.current && modalRef.current.contains(event.target);
 
       // Check if click is inside any open submenu
       let clickedInsideMenu = false;
-      if (activeMenu === 'study') {
-        clickedInsideMenu = studyMenuRef.current && studyMenuRef.current.contains(event.target);
-      } else if (activeMenu === 'application') {
-        clickedInsideMenu = applicationMenuRef.current && applicationMenuRef.current.contains(event.target);
-      } else if (activeMenu === 'coding') {
-        clickedInsideMenu = codingMenuRef.current && codingMenuRef.current.contains(event.target);
+      if (activeMenu === "study") {
+        clickedInsideMenu =
+          studyMenuRef.current && studyMenuRef.current.contains(event.target);
+      } else if (activeMenu === "application") {
+        clickedInsideMenu =
+          applicationMenuRef.current &&
+          applicationMenuRef.current.contains(event.target);
+      } else if (activeMenu === "coding") {
+        clickedInsideMenu =
+          codingMenuRef.current && codingMenuRef.current.contains(event.target);
       }
-      
 
       // If click is outside both Profile Modal and any open submenu, close them
       if (!clickedInsideProfile && !clickedInsideMenu) {
         setShowProfileModal(false);
-        setActiveMenu('');
+        setActiveMenu("");
       }
     };
 
     // Attach the event listener
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     // Cleanup the event listener on component unmount
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [showProfileModal, activeMenu]);
 
@@ -88,33 +92,62 @@ export default function Header() {
       <itemS.InnerContainer>
         <itemS.HeaderWrap>
           <itemS.HeaderLeftWrap>
-            <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} style={{textDecoration: 'none'}}><itemS.Rabel src='/img/koalalogo.png' alt='코알라로고'/></itemS.StyledLink>
+            <itemS.StyledLink
+              to={isLoggedIn ? "/regularstudylist" : "/login"}
+              style={{ textDecoration: "none" }}
+            >
+              <itemS.Rabel src="/img/koalalogo.png" alt="코알라로고" />
+            </itemS.StyledLink>
           </itemS.HeaderLeftWrap>
           <itemS.HeaderRightWrap>
-            <itemS.StyledLink onClick={() => handleMenuClick('application')}>
+            <itemS.StyledLink onClick={() => handleMenuClick("application")}>
               <itemS.PageLink>지원자 관리</itemS.PageLink>
-              {activeMenu === 'application' && (
+              {activeMenu === "application" && (
                 <itemS.SubMenuContaier ref={applicationMenuRef}>
                   <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                    <itemS.StyledLink to={isLoggedIn ? "/manageauth" : "/login"} onClick={(e) => {e.stopPropagation(); handleNav();}}>
+                    <itemS.StyledLink
+                      to={isLoggedIn ? "/manageauth" : "/login"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleNav();
+                      }}
+                    >
                       <itemS.SubMenuItem>권한 관리</itemS.SubMenuItem>
                     </itemS.StyledLink>
-                    <itemS.StyledLink to={isLoggedIn ? "/answer" : "/login"} onClick={(e) => {e.stopPropagation(); handleNav();}}>
+                    <itemS.StyledLink
+                      to={isLoggedIn ? "/answer" : "/login"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleNav();
+                      }}
+                    >
                       <itemS.SubMenuItem>지원자 관리</itemS.SubMenuItem>
                     </itemS.StyledLink>
                   </itemS.SubMenu>
                 </itemS.SubMenuContaier>
               )}
-            </itemS.StyledLink> 
-            <itemS.StyledLink onClick={() => handleMenuClick('study')}>
+            </itemS.StyledLink>
+            <itemS.StyledLink onClick={() => handleMenuClick("study")}>
               <itemS.PageLink>스터디 관리</itemS.PageLink>
-              {activeMenu === 'study' && (
+              {activeMenu === "study" && (
                 <itemS.SubMenuContaier ref={studyMenuRef}>
                   <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                    <itemS.StyledLink to={isLoggedIn ? "/regularstudylist" : "/login"} onClick={(e) => {e.stopPropagation(); handleNav();}}>
+                    <itemS.StyledLink
+                      to={isLoggedIn ? "/regularstudylist" : "/login"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleNav();
+                      }}
+                    >
                       <itemS.SubMenuItem>정규 스터디</itemS.SubMenuItem>
                     </itemS.StyledLink>
-                    <itemS.StyledLink to={isLoggedIn ? "/generation" : "/login"} onClick={(e) => {e.stopPropagation(); handleNav();}}>
+                    <itemS.StyledLink
+                      to={isLoggedIn ? "/generation" : "/login"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleNav();
+                      }}
+                    >
                       <itemS.SubMenuItem>기수 갱신</itemS.SubMenuItem>
                     </itemS.StyledLink>
                   </itemS.SubMenu>
@@ -127,29 +160,43 @@ export default function Header() {
             {/* <itemS.StyledLink>
               <itemS.PageLink>문의 관리</itemS.PageLink>
             </itemS.StyledLink> */}
-            <itemS.StyledLink onClick={() => handleMenuClick('coding')}>
+            <itemS.StyledLink onClick={() => handleMenuClick("coding")}>
               <itemS.PageLink>코딩테스트 분석</itemS.PageLink>
-              {activeMenu === 'coding' && (
+              {activeMenu === "coding" && (
                 <itemS.SubMenuContaier ref={codingMenuRef}>
                   <itemS.SubMenu isLoggedIn={isLoggedIn}>
-                    <itemS.StyledLink to={isLoggedIn ? "/enterbootlist" : "/login"} onClick={(e) => {e.stopPropagation(); handleNav();}}>
-                      <itemS.SubMenuItem style={{marginBottom:"2.5rem"}}>기업/부트캠프</itemS.SubMenuItem>
+                    <itemS.StyledLink
+                      to={isLoggedIn ? "/enterbootlist" : "/login"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleNav();
+                      }}
+                    >
+                      <itemS.SubMenuItem style={{ marginBottom: "2.5rem" }}>
+                        기업/부트캠프
+                      </itemS.SubMenuItem>
                     </itemS.StyledLink>
                   </itemS.SubMenu>
                 </itemS.SubMenuContaier>
               )}
             </itemS.StyledLink>
             {isLoggedIn ? (
-              <div style={{ position: 'relative' }} ref={modalRef}>
+              <div style={{ position: "relative" }} ref={modalRef}>
                 <itemS.ProfileBox onClick={toggleProfileModal}>
                   <itemS.AdminName>안녕하세요, {userName} 님</itemS.AdminName>
-                  <itemS.Arrow src='/img/arrow-bt.svg' alt='화살표' />
+                  <itemS.Arrow src="/img/arrow-bt.svg" alt="화살표" />
                 </itemS.ProfileBox>
-                {showProfileModal && <ProfileModal userName={userName} profileUrl={profileUrl} setIsLoggedIn={setIsLoggedIn}/>}
+                {showProfileModal && (
+                  <ProfileModal
+                    userName={userName}
+                    profileUrl={profileUrl}
+                    setIsLoggedIn={setIsLoggedIn}
+                  />
+                )}
               </div>
             ) : (
               <itemS.StyledLink to="/login">
-                <itemS.Btn>로그인/회원가입</itemS.Btn>
+                <itemS.Btn>로그인</itemS.Btn>
               </itemS.StyledLink>
             )}
           </itemS.HeaderRightWrap>
